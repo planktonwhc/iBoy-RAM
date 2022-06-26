d8824929ce16b7ee4456a774fb311a89b3fbe742
PLANKTON <PLANKTON@PLANKTON-PC>
2022-06-26 16:46:35 +0700

    Add project files.

---
+++ iBoy-RAM/iBoy.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace iBoy_RAM
+{
+    partial class iBoy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(iBoy));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_mode = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_status = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txt_Type = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_ECID = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_model = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_checkdev = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.panel_log = new System.Windows.Forms.Panel();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 316);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(618, 10);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_mode);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.txt_status);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.txt_Type);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txt_ECID);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txt_model);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(300, 146);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "iDevice Info";
+            // 
+            // txt_mode
+            // 
+            this.txt_mode.AutoSize = true;
+            this.txt_mode.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_mode.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txt_mode.Location = new System.Drawing.Point(206, 63);
+            this.txt_mode.Name = "txt_mode";
+            this.txt_mode.Size = new System.Drawing.Size(32, 13);
+            this.txt_mode.TabIndex = 9;
+            this.txt_mode.Text = "DFU";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(160, 63);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "MODE :";
+            // 
+            // txt_status
+            // 
+            this.txt_status.AutoSize = true;
+            this.txt_status.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_status.ForeColor = System.Drawing.Color.Lime;
+            this.txt_status.Location = new System.Drawing.Point(206, 36);
+            this.txt_status.Name = "txt_status";
+            this.txt_status.Size = new System.Drawing.Size(87, 13);
+            this.txt_status.TabIndex = 7;
+            this.txt_status.Text = "REGISTERED";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(160, 36);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(46, 13);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "Status  :";
+            // 
+            // txt_Type
+            // 
+            this.txt_Type.AutoSize = true;
+            this.txt_Type.Location = new System.Drawing.Point(54, 88);
+            this.txt_Type.Name = "txt_Type";
+            this.txt_Type.Size = new System.Drawing.Size(48, 13);
+            this.txt_Type.TabIndex = 5;
+            this.txt_Type.Text = "txt_Type";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 88);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Type  :";
+            // 
+            // txt_ECID
+            // 
+            this.txt_ECID.AutoSize = true;
+            this.txt_ECID.Location = new System.Drawing.Point(54, 63);
+            this.txt_ECID.Name = "txt_ECID";
+            this.txt_ECID.Size = new System.Drawing.Size(49, 13);
+            this.txt_ECID.TabIndex = 3;
+            this.txt_ECID.Text = "txt_ECID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ECID  :";
+            // 
+            // txt_model
+            // 
+            this.txt_model.AutoSize = true;
+            this.txt_model.Location = new System.Drawing.Point(54, 36);
+            this.txt_model.Name = "txt_model";
+            this.txt_model.Size = new System.Drawing.Size(52, 13);
+            this.txt_model.TabIndex = 1;
+            this.txt_model.Text = "txt_model";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Model  :";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_checkdev);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Location = new System.Drawing.Point(12, 164);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(147, 146);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ramdisk Menu";
+            // 
+            // btn_checkdev
+            // 
+            this.btn_checkdev.Location = new System.Drawing.Point(10, 28);
+            this.btn_checkdev.Name = "btn_checkdev";
+            this.btn_checkdev.Size = new System.Drawing.Size(130, 23);
+            this.btn_checkdev.TabIndex = 4;
+            this.btn_checkdev.Text = "Check Device (DFU)";
+            this.btn_checkdev.UseVisualStyleBackColor = true;
+            this.btn_checkdev.Click += new System.EventHandler(this.check_device);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(10, 57);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "BOOT 1";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(10, 86);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "BOOT 2";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(10, 115);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(130, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Check SSH";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button5);
+            this.groupBox3.Controls.Add(this.button6);
+            this.groupBox3.Controls.Add(this.button7);
+            this.groupBox3.Controls.Add(this.button8);
+            this.groupBox3.Location = new System.Drawing.Point(165, 164);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(147, 146);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "TOOLS";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(10, 28);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(130, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Backup Activation";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(10, 57);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(130, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Restore Activation";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(10, 86);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(130, 23);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Owner Info";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(10, 115);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(130, 23);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Fix Notification";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // panel_log
+            // 
+            this.panel_log.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel_log.Location = new System.Drawing.Point(319, 18);
+            this.panel_log.Name = "panel_log";
+            this.panel_log.Size = new System.Drawing.Size(310, 289);
+            this.panel_log.TabIndex = 9;
+            // 
+            // iBoy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(640, 335);
+            this.Controls.Add(this.panel_log);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "iBoy";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "iBoy - Ramdisk";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_checkdev;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Panel panel_log;
+        private System.Windows.Forms.Label txt_ECID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label txt_model;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label txt_Type;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label txt_status;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label txt_mode;
+    }
+}
+
---
+++ iBoy-RAM/iBoy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace iBoy_RAM
+{
+    public partial class iBoy : Form
+    {
+        public iBoy()
+        {
+            InitializeComponent();
+        }
+
+        private void check_device(object sender, EventArgs e)
+        {
+			using (Process process = new Process())
+			{
+				process.StartInfo.FileName = Environment.CurrentDirectory + "/files/irecovery.exe";
+				process.StartInfo.Arguments = "-q";
+				process.StartInfo.UseShellExecute = false;
+				process.StartInfo.RedirectStandardOutput = true;
+				process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+				process.StartInfo.CreateNoWindow = true;
+				process.Start();
+				Text = process.StandardOutput.ReadToEnd();
+				process.WaitForExit();
+			}
+			if (Text.Contains("Error"))
+			{
+				MessageBox.Show("Please unlock your device and press Trust !", "NOT ERROR", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+			}
+			
+			
+		}
+    }
+}
